0d12a0ec7f5d2465878f4a0a38e219bf94f4ddec
clastr <provazradek@gmail.com>
2021-12-14 18:33:52 +0100

    book repository @query

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -2,9 +2,12 @@ package guru.springframework.jdbc.repositories;
 
 import guru.springframework.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.Optional;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -16,4 +19,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book getByTitle(@Nullable String title);
 
     Stream<Book> findAllByTitleNotNull();
+
+    @Async
+    Future<Book> queryByTitle(String title);
+
+    @Query("select b from Book b where b.title = ?1")
+    Book findBookByTitleWithQuery(String string);
 }
